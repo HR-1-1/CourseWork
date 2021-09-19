959ab4015c5a6275d4e26d7e8f85ac967aa9b806
Harish R <76567644+HR-1-1@users.noreply.github.com>
2021-09-19 23:12:52 +0530

    Indentation

--- EE2016/MISCELS/FibonacciNum.asm
+++ EE2016/MISCELS/FibonacciNum.asm
@@ -14,24 +14,24 @@
 		LDI		R21,	HIGH(RAMEND)
 		OUT		SPH,	R21
 
-		LDI		ZL,		LOW(NUM<<1)
-		LDI		ZH,		HIGH(NUM<<1)
+		LDI		ZL,	LOW(NUM<<1)
+		LDI		ZH,	HIGH(NUM<<1)
 
-		LPM		R1,		Z+
-		LPM		R2,		Z
+		LPM		R1,	Z+
+		LPM		R2,	Z
 
-		LDI		ZL,		0x0B
-		LDI		ZH,		0x00
+		LDI		ZL,	0x0B
+		LDI		ZH,	0x00
 
 		LDI		R23,	0x0A
 
 loop:	PUSH	R2
-		ADD		R2,		R1
-		ST		Z+,		R1
+		ADD		R2,	R1
+		ST		Z+,	R1
 		POP		R1
 		DEC		R23
 		BRNE	loop
 
 vetti:	RJMP	vetti
 		
-NUM:	.DB		0x00,	0x01
+NUM:	.DB	0x00,	0x01
